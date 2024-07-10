9df0db6bb1ec16a1686de57a6b23747548afcdf5
Eshfield <heshfield@gmail.com>
2024-07-10 18:30:24 +0300

    Update generated files

--- lib/data/sources/remote/tasks_api.g.dart
+++ lib/data/sources/remote/tasks_api.g.dart
@@ -6,7 +6,7 @@ part of 'tasks_api.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
 
 class _TasksApi implements TasksApi {
   _TasksApi(
@@ -43,8 +43,8 @@ class _TasksApi implements TasksApi {
               _dio.options.baseUrl,
               baseUrl,
             ))));
-    final value = TasksDto.fromJson(_result.data!);
-    return value;
+    final _value = TasksDto.fromJson(_result.data!);
+    return _value;
   }
 
   @override
@@ -75,8 +75,8 @@ class _TasksApi implements TasksApi {
               _dio.options.baseUrl,
               baseUrl,
             ))));
-    final value = TasksDto.fromJson(_result.data!);
-    return value;
+    final _value = TasksDto.fromJson(_result.data!);
+    return _value;
   }
 
   @override
@@ -107,8 +107,8 @@ class _TasksApi implements TasksApi {
               _dio.options.baseUrl,
               baseUrl,
             ))));
-    final value = TaskDto.fromJson(_result.data!);
-    return value;
+    final _value = TaskDto.fromJson(_result.data!);
+    return _value;
   }
 
   @override
@@ -140,8 +140,8 @@ class _TasksApi implements TasksApi {
               _dio.options.baseUrl,
               baseUrl,
             ))));
-    final value = TaskDto.fromJson(_result.data!);
-    return value;
+    final _value = TaskDto.fromJson(_result.data!);
+    return _value;
   }
 
   @override
@@ -171,8 +171,8 @@ class _TasksApi implements TasksApi {
               _dio.options.baseUrl,
               baseUrl,
             ))));
-    final value = TaskDto.fromJson(_result.data!);
-    return value;
+    final _value = TaskDto.fromJson(_result.data!);
+    return _value;
   }
 
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
